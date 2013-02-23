4eaa10e02bef2c7ffc4ffd4084baaff8ba1262c8
ASMlover <asmlover@126.com>
2013-02-23 19:57:42 +0800

    added el_thread modeule.

---
+++ elib/inc/el_thread.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *    notice, this list of conditions and the following disclaimer in
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __ELIB_THREAD_HEADER_H__
+#define __ELIB_THREAD_HEADER_H__
+
+#if (defined(_WIN32) || defined(_WIN64))
+  #include "./win32/el_win32_thread.h"
+#else 
+  #include "./posix/el_posix_thread.h"
+#endif 
+#include "el_common.h"
+
+extern int32_t el_thread_create(el_thread_t* tid, 
+    void (*routine)(void* arg), void* arg);
+extern uint32_t el_thread_self(void);
+extern int32_t el_thread_join(el_thread_t* tid);
+
+#endif  /* __ELIB_THREAD_HEADER_H__ */
---
+++ elib/src/posix/el_posix_thread.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *    notice, this list of conditions and the following disclaimer in
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "../../inc/el_error.h"
+#include "../../inc/el_thread.h"
+
+
+typedef struct el_context_s {
+  void (*routine)(void* arg);
+  void* arg;
+} el_context_t;
+
+
+static void* 
+el_thread_routine(void* ctx_arg) 
+{
+  void (*routine)(void* arg);
+  void* arg;
+  el_context_t* ctx = (el_context_t*)ctx_arg;
+
+  routine = ctx->routine;
+  arg = ctx->arg;
+  free(ctx);
+
+  routine(arg);
+
+  return NULL;
+}
+
+
+
+int32_t 
+el_thread_create(el_thread_t* tid, void (*routine)(void* arg), void* arg)
+{
+  el_context_t* ctx;
+
+  if (NULL == (ctx = (el_context_t*)malloc(sizeof(*ctx)))) 
+    return EL_NO;
+  ctx->routine = routine;
+  ctx->arg = arg;
+
+  if (0 != pthread_create(tid, NULL, el_thread_routine, ctx)) {
+    free(ctx);
+    return EL_NO;
+  }
+
+  return EL_OK;
+}
+
+uint32_t 
+el_thread_self(void) 
+{
+  return (uint32_t)pthread_self();
+}
+
+int32_t 
+el_thread_join(el_thread_t* tid)
+{
+  if (0 != pthread_join(*tid, NULL))
+    return EL_NO;
+  else 
+    return EL_OK;
+}
