07c1e11ef74aae3cc5f99c4bedc37f8ecdd2de08
ASMlover <asmlover@126.com>
2013-09-20 16:51:53 +0800

    modified test header for elib

--- test/el_test_header.h
+++ test/el_test_header.h
@@ -29,9 +29,9 @@
 #ifndef __ELIB_TEST_HEADER_H__
 #define __ELIB_TEST_HEADER_H__
 
-#if (defined(_WIN32) || defined(_WIN64))
-  #define __func__  __FUNCTION__
-#endif 
+#ifndef ELIB_COMMON
+# include <el_common.h>
+#endif
 
 extern void el_test_time(void* arg);
 
